08b2fb92a3f0aa9f96251e7a7eb5923b69a603f1
gwaDyckuL1 <gwaDyckuL1@yahoo.com>
2025-12-02 12:00:37 -0800

    First commit

---
+++ day_1.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func Part1(data []string) int {
+	fmt.Println(data)
+
+	return 0
+}
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var Problem int
+var Path string
+
+func main() {
+	Problem = 1
+	Path = "/imports/day1_Example.txt"
+
+	data := ImportFile(Path)
+
+	switch Problem {
+	case 1:
+		fmt.Println("The password is: ", Part1(data))
+	}
+
+}
+
+func ImportFile(path string) []string {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error opening file at ", path)
+		panic(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	data := []string{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		data = append(data, line)
+	}
+
+	return data
+}
